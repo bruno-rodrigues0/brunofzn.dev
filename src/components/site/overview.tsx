5f62b0651bf4243589ab4965187e4b8fd56738f3
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-09-08 10:36:31 -0300

    internationalize overview

--- src/components/site/overview.tsx
+++ src/components/site/overview.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { Clock, CodeXml, Link, LucideProps, Mail, MapPin, Mars, Phone} from "lucide-react";
+import { useTranslations } from "next-intl";
 import { ReactNode, useEffect, useState } from "react";
 
 const localeOpts: Intl.DateTimeFormatOptions = {timeZone: 'America/Sao_Paulo', hour: 'numeric', minute: 'numeric'}
 
 export default function Overview(){
-
+  const t = useTranslations("overview")
   const [time, setTime] = useState(new Date().toLocaleTimeString("pt-BR", localeOpts))
 
   useEffect(() => {
@@ -21,20 +22,20 @@ export default function Overview(){
     <section className="border-x border-line pt-12 p-4 grid grid-cols-2 max-sm:grid-cols-1 gap-4">
         <div>
           <dl className="flex flex-col gap-4 font-sans">
-              <OverviewItem term="Job" text="Computer Scientist: CIn - UFPE"><CodeXml size={15}/></OverviewItem>
-              <OverviewItem term="Location" text="Recife - PE, Brazil"><MapPin size={15}/></OverviewItem>
-              <OverviewItem term="Email" text="brunorodriguesmtv0@gmail.com"><Mail size={15}/></OverviewItem>
-              <OverviewItem term="Phone number" text="+55 38 99737 5691"><Phone size={15}/></OverviewItem>
+              <OverviewItem term={t("job")} text={t("jobValue")}><CodeXml size={15}/></OverviewItem>
+              <OverviewItem term={t("location")} text={t("locationValue")}><MapPin size={15}/></OverviewItem>
+              <OverviewItem term={t("email")} text={t("emailValue")}><Mail size={15}/></OverviewItem>
+              <OverviewItem term={t("phone")} text={t("phoneValue")}><Phone size={15}/></OverviewItem>
               <OverviewItem term="Site" text="brunofzn.dev"><Link size={15}/></OverviewItem>
           </dl>
         </div>
 
         <div className="flex items-end">
           <dl className="flex flex-col gap-4 font-mono">
-            <OverviewItem term="Time" text={(
+            <OverviewItem term={t("time")} text={(
               <>{time} <span className="text-muted-foreground">(GMT-03)</span></>
             )}><Clock size={15}/></OverviewItem>
-            <OverviewItem term="Pronouns" text="he/him"><Mars size={15}/></OverviewItem>
+            <OverviewItem term={t("pronouns")} text={t("pronounsValue")}><Mars size={15}/></OverviewItem>
           </dl>
         </div>
     </section>
