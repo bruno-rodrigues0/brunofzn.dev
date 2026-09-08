c8f3e804fc4eff2b68c30aebddaa5e9bd55d017e
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-09-08 00:55:08 -0300

    layout and metadata updated with next-intl

---
+++ src/app/[locale]/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import { NameGradient } from "@/components/site/name-gradient";
+import { ScrollToTop } from "@/components/scroll-to-top";
+import { SiteFooter } from "@/components/site/site-footer";
+import SiteHeader from "@/components/site/site-header";
+
+export default function HomeLayout({ children }: LayoutProps<"/[locale]">) {
+  return (
+    <div className="relative isolate">
+      <SiteHeader/>
+      <main className="max-w-screen px-2"> {children} </main>
+      <SiteFooter /> 
+      <NameGradient/>
+      <ScrollToTop className="w-8 h-8"/>
+    </div>
+  )
+}
+
