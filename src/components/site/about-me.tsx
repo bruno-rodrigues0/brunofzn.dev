9861203900e8c55903a042eccde2158b7e8fd01f
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-09-11 14:59:41 -0300

    add animation to show more in about section

--- src/components/site/about-me.tsx
+++ src/components/site/about-me.tsx
@@ -5,6 +5,8 @@ import { useTranslations } from "next-intl"
 import { Button } from "../ui/button"
 import { Separator } from "../ui/separator"
 import { ChevronDown, ChevronUp } from "lucide-react"
+import { AnimatePresence } from "motion/react"
+import { motion } from "motion/react"
 
 export default function AboutMe(){
   const t = useTranslations('about')
@@ -14,6 +16,7 @@ export default function AboutMe(){
   }))
 
   const [showMore, setShowMore] = useState<boolean>(false)
+  const paragraphs = t.raw('paragraphs') as string[]
 
   const getGreeting = () => {
     if (time >= 6 && time < 12) {
@@ -36,14 +39,29 @@ export default function AboutMe(){
       <Separator className="absolute left-0"/>
 
       <div className="py-4">
-        <ul className="list-disc marker:text-zinc-700  marker:text-lg space-y-3 list-inside">
-          {(t.raw('paragraphs') as string[]).map((phrase, index) => {
-            if ((index > 2 && showMore) || index <= 2) {
-              return (<li key={index} className="text-sm font-medium">{phrase}</li>)
-            }
-          }
-          )}
+        <ul className="list-disc marker:text-zinc-700  marker:text-lg space-y-3 list-inside transition-[height]">
+          {paragraphs.slice(0, 3).map((phrase, index) => (
+            <li key={index} className="text-sm font-medium">{phrase}</li> 
+          ))}
         </ul>
+
+        <AnimatePresence>
+          {showMore &&
+            <motion.ul
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              exit={{ opacity: 0, height: 0 }}
+              transition={{ duration: .2 }}
+              className="list-disc marker:text-zinc-700  marker:text-lg space-y-3 list-inside mt-2"
+            >
+              {paragraphs.slice(3).map((phrase, index) => (
+                <li key={index} className="text-sm font-medium overflow-hidden">
+                  {phrase}
+                </li>
+              ))}
+            </motion.ul>
+          }
+        </AnimatePresence>
       </div>
 
       <Separator className="absolute left-0" />
