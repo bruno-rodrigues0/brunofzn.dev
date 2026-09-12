922da5d3a3e68e9b18be126240d54df692ce0319
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-09-12 15:36:54 -0300

    feat: avatar lights toggle

--- src/components/site/profile-header.tsx
+++ src/components/site/profile-header.tsx
@@ -1,6 +1,4 @@
 "use client"
-import Image from "next/image";
-import Me from "@public/me.webp"
 import { Verified, MousePointerClick} from "lucide-react";
 import { TextFlip } from "../text-flip";
 import { Separator } from "../ui/separator";
@@ -13,6 +11,8 @@ import { click005Sound } from "../../lib/click-005";
 import { click004Sound } from "../../lib/click-004";
 import { VerifiedSolidIcon } from "../icons";
 import { cn } from "../../lib/utils";
+import { AvatarLightsToggle } from "../avatar-lights-toggle";
+import { AvatarLights } from "../avatar-lights";
 
 const COUNTER_STORAGE_KEY = "counter"
 
@@ -58,7 +58,7 @@ export default function ProfileHeader() {
   return (
     <section id="profile-header" className="screen-line-bottom grid grid-cols-[auto_1fr] grid-rows-[1fr_auto] overflow-y-clip border-x border-line">
       <div className=" w-full h-87 max-sm:h-52 col-span-2 p-2 sm:col-span-1 sm:col-start-2 sm:p-4">
-        <figure className="absolute w-xl max-sm:w-full max-sm:max-w-80 h-80 max-sm:h-48 top-1.5 left-[54%] max-sm:left-1/2 -translate-x-1/2 col-span-2 p-2 z-20 flex items-center justify-center">
+        <figure className="absolute w-xl max-sm:w-full max-sm:max-w-80 h-80 max-sm:h-48 top-1.5 left-[55%] max-sm:left-1/2 -translate-x-1/2 col-span-2 p-2 z-20 flex items-center justify-center">
 
           <div>
             <div className={cn(
@@ -78,21 +78,19 @@ export default function ProfileHeader() {
           <div className="w-full h-full max-sm:max-h-25 z-20">
             <SpotlightLogo onClick={handleCounterInc} className="w-145 absolute top-0 -left-9 max-sm:w-84 max-sm:-left-2"/>
           </div>
-          <figcaption className="pointer-events-none absolute right-1 bottom-0 text-sm leading-none tracking-wide text-zinc-600 tabular-nums select-none">Fig. 1</figcaption>
+          <figcaption className="pointer-events-none absolute right-10 bottom-0 text-sm leading-none tracking-wide text-zinc-600 tabular-nums select-none">Fig. 1</figcaption>
         </figure>
       </div>
 
       <div className="flex flex-col sm:row-span-2 sm:row-start-1">
-        <div className="screen-line-top mt-auto shrink-0 border-r border-line">
-          <Separator className="absolute left-0 w-screen -z-10"/>
-          <div className="group/avatar-lights-toggle mx-0.5 my-0.75 flex outline-none">
-            <Image
-              className="ring-border ring-offset-background rounded-full w-33 h-33 max-sm:w-28 max-sm:h-28 p-1 z-20"
-              loading="eager"
-              src={Me}
-              alt="Photo of Bruno Silva"
-            />
-          </div >
+        <div className="mt-auto shrink-0 border-r border-line">
+          <Separator className="absolute left-0 w-screen z-10"/>
+            <AvatarLightsToggle className="relative group/avatar-lights-toggle mx-0.5 my-0.75 flex outline-none z-50">
+              <AvatarLights
+                className="ring-border ring-offset-background group-focus-visible/avatar-lights-toggle:ring-1 group-focus-visible/avatar-lights-toggle:ring-offset-2 z-20"
+                variants={USER.avatarVariants}
+              />
+            </AvatarLightsToggle>
         </div>
       </div>
 
